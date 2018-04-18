b78b04bc3e5e0a64b52791af90fd0c19835714b6
Khavel <Cejatr0n>
2018-04-18 12:22:33 +0200

    Ejercicio banco

---
+++ src/threads/Banco/CuentaBanco.java
@@ -0,0 +1,17 @@
+package threads.Banco;
+
+public class CuentaBanco {
+    private int balance = 50;
+
+    public CuentaBanco(){
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public void retiroBancario(int retiro){
+        balance = balance - retiro;
+    }
+
+}
